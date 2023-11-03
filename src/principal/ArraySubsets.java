fc2b604e98c124116a2f193252a2098b76990673
Vinícius Guimarães <vinijipa23@gmail.com>
2023-11-03 14:53:09 -0300

    array subsets

--- src/principal/ArraySubsets.java
+++ src/principal/ArraySubsets.java
@@ -1,12 +1,15 @@
 package principal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArraySubsets {
 	
 	public static void main(String[] args) {
 		
 		int myArray[] = new int[4];
 		for (int i = 0; i < myArray.length; i++) {
-			myArray[i] = i; 
+			myArray[i] = (i+1); 
 		}
 		
 		int number = (int) Math.pow(2, myArray.length);
@@ -17,7 +20,20 @@ public class ArraySubsets {
 			// Em seguida, fazemos o replace colocando 0's no lugar dos espaços
 			String binaryStringFormatted = binaryString.format("%" + myArray.length + "s", binaryString)
 					.replace(' ', '0');
+			System.out.println("");
 			System.out.println("myString: " + binaryStringFormatted);
+			
+			char[] myCharArray = binaryStringFormatted.toCharArray();
+			
+			List<Integer> myList = new ArrayList<>();
+			for (int j = 0; j < myCharArray.length; j++) {
+				if (myCharArray[j] == '1') { 
+					myList.add(myArray[j]);
+				}
+			}	
+			
+			myList.forEach(e -> { System.out.print(e + " "); });
+			System.out.println("");
 		}
 	}
 }
